0814a72010f4824005e20b14f388e7e1b4054398
mutugi-c <mutugichris@gmail.com>
2023-03-27 22:01:18 +0200

    Display time using luxon.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import { DateTime } from './node_modules/luxon/build/es6/luxon.js';
 import Library from './modules/library.js';
 import './modules/navigation.js';
+
 
 const library = new Library();
 library.displayBooks();
@@ -18,7 +20,6 @@ document.getElementById('add-book-btn').addEventListener('click', (event) => {
 
 // Display current time
 const currentTime = document.getElementById('time');
-const now = new Date();
-const date = now.toDateString();
-const time = now.toLocaleTimeString();
-currentTime.innerHTML = `${date} ${time}`;
+const date = DateTime.now();
+const dateTime = date.toFormat('EEE d MMM yyyy h:mm:ss a');
+currentTime.innerHTML = dateTime;
